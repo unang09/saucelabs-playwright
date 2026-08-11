ab3838fe2504a171bacf8ab26a42968314649897
Unang <unang@softwareseni.com>
2026-08-11 16:15:44 +0700

    Update Playwright configuration to use environment variables for base URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { env } from './resources/config/env.config';
 
 /**
  * Read environment variables from file.
@@ -26,9 +27,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    testIdAttribute: 'data-test',
     trace: 'on-first-retry',
   },
 
